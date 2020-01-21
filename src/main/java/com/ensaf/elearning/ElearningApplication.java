4f7648cfb87b6acc73a418d31ddac0e44e36df0e
abdellah <abdel.ysf@gmail.com>
2020-01-21 02:36:24 +0100

    modifying spring security login redirection

--- src/main/java/com/ensaf/elearning/ElearningApplication.java
+++ src/main/java/com/ensaf/elearning/ElearningApplication.java
@@ -2,8 +2,9 @@ package com.ensaf.elearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ElearningApplication {
 
     public static void main(String[] args) {
